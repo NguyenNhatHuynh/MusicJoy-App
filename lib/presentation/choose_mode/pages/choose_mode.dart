7d4383d1b95f9a8cd30bc7dd77134a51849fa232
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-11-15 17:24:26 +0700

    Design Choose Mode v1

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,10 +1,128 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:musicjoy/common/widgets/button/basic_app_button.dart';
+import 'package:musicjoy/core/configs/assets/app_images.dart';
+import 'package:musicjoy/core/configs/assets/app_vectors.dart';
+import 'package:musicjoy/core/configs/theme/app_colors.dart';
 
 class ChooseModePage extends StatelessWidget {
   const ChooseModePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.introBG),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xff30393C).withOpacity(0.5),
+                          ),
+                          child: SvgPicture.asset(
+                            AppVectors.moon,
+                            fit: BoxFit.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    ClipOval(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xff30393C).withOpacity(0.5),
+                          ),
+                          child: SvgPicture.asset(
+                            AppVectors.sun,
+                            fit: BoxFit.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  'Welcome to Music Joy - your world of music! Discover, enjoy, and immerse yourself in every favorite melody. Start your journey now!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 13,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            const ChooseModePage(),
+                      ),
+                    );
+                  },
+                  title: 'Get Started',
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
