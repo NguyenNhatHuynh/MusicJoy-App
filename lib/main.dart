a0e3199ba03c3ad75316d484c7c3b085e4c2b000
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-11-30 13:08:22 +0700

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:musicjoy/core/configs/theme/app_theme.dart';
+import 'package:musicjoy/firebase_options.dart';
 import 'package:musicjoy/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:musicjoy/presentation/splash/pages/splash.dart';
 import 'package:path_provider/path_provider.dart';
@@ -14,6 +16,7 @@ Future<void> main() async {
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
   );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
